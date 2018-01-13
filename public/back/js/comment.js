771aeedddcabdd2b7ddf8b16add08565ca878856
lcycq <you@example.com>
2018-01-13 08:50:02 +0800

    完成首页

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -3,8 +3,9 @@
  */
 
 
-$(function(){
+//页面加载进度条逻辑
 
+$(function(){
 //关闭nprogress的进度环
     NProgress.configure({ showSpinner: false });
 
@@ -17,6 +18,65 @@ $(function(){
             NProgress.done();
         },1000)
     })
+});
+
+//侧边栏的显示隐藏逻辑
+$(function(){
+    $('#qp').on('click',function(){
+        $('.lt_aside').toggleClass('hid');
+        $('.lt_main').toggleClass('sh');
+    });
+});
+
+//页面退出逻辑
+$(function(){
+
+    $('#out').on('click',function(){
+        //显示模态框
+        $('#logoutModal').modal({
+          show:true
+        });
+    });
+
+    $('.btn-logout').off().on('click',function(){
+       //退出
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            success: function(info){
+                if(info.success){
+                    location.href = 'login.html';
+                }
+            }
+        })
+
+    });
 
 
+});
+
+//判断用户是否登陆
+$(function(){
+
+    if(location.href.indexOf('login.html') == -1){
+        $.ajax({
+            type: 'get',
+            url: '/employee/checkRootLogin',
+            success: function(info){
+                if(info.error){
+                    location.href = 'login.html';
+                }
+            }
+
+        })
+    }
+
+
+});
+
+//二级菜单的显示与隐藏
+$(function(){
+   $('.second').prev().on('click',function(){
+       $(this).next().slideToggle();
+   })
 });
--- public/back/js/index.js
+++ public/back/js/index.js
@@ -1,3 +1,73 @@
 /**
  * Created by Administrator on 2018-1-11.
  */
+
+$(function(){
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('left'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: '2017年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data:['人数']
+        },
+        xAxis: {
+            data: ["1月","2月","3月","4月","5月","6月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+
+
+    var myChart1 = echarts.init(document.getElementById('right'));
+    option1 = {
+        title : {
+            text: '热门品牌销售',
+            subtext: '2017年6月',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['耐克','阿迪','李宁','新百伦','阿迪王']
+        },
+        series : [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '60%'],
+                data:[
+                    {value:335, name:'耐克'},
+                    {value:310, name:'阿迪'},
+                    {value:234, name:'李宁'},
+                    {value:135, name:'新百伦'},
+                    {value:1548, name:'阿迪王'}
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    myChart1.setOption(option1);
+});
