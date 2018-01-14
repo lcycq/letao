82e5e9c609d10b0ec712c87ec68a953994e24922
lcycq <you@example.com>
2018-01-14 20:57:19 +0800

    移动端首页完成

---
+++ public/front/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2018-1-14.
+ */
+
+$(function(){
+    //初始化轮播图的自动翻页
+    mui('.mui-slider').slider({
+        interval:1000
+    });
+    //初始化页面区域滚动
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+
+});
