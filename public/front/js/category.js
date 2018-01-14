bd43d6c0daa939c007881d7140bb3156cabdc22b
lcycq <you@example.com>
2018-01-14 22:05:25 +0800

    category完成

---
+++ public/front/js/category.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Administrator on 2018-1-14.
+ */
+
+$(function(){
+    //渲染左边li的数据
+    function renderLeft(){
+        $.ajax({
+            type: 'get',
+            url: '/category/queryTopCategory',
+            success: function(info){
+                $('.category-left ul').html(template('categoryTpl',info));
+            }
+        })
+    }
+    renderLeft();
+
+    //渲染右边li的数据
+    function renderRight(id){
+        $.ajax({
+            type: 'get',
+            url: '/category/querySecondCategory',
+            data: {id:id},
+            success: function(info){
+                console.log(info);
+                $('.category-right ul').html(template('cTpl',info));
+            }
+        })
+    }
+    renderRight(1);
+    //给左边li添加点击事件
+    $('.category-left').on('click','li',function(){
+        //添加active类
+        $(this).addClass('active').siblings().removeClass('active');
+        //获取发送二级分类的数据
+        var id = $(this).data('id');
+        //重新渲染页面
+        renderRight(id);
+    })
+});
--- public/front/js/comment.js
+++ public/front/js/comment.js
@@ -1,3 +1,12 @@
 /**
  * Created by Administrator on 2018-1-14.
  */
+
+$(function(){
+
+    //初始化页面区域滚动
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+})
