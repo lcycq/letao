eed329f4a95dda8b8269c0b042d8b382833e9e5b
lcycq <you@example.com>
2018-01-14 18:57:54 +0800

    second暂停

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -15,10 +15,31 @@ $(function(){
             },
             success:function(info){
                 console.log(info);
-            }
+                var result = template('secondTpl',info);
+                $('tbody').html(result);
 
+                //完成分页
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion:3,
+                    currentPage:page,
+                    totalPages:info.total/pageSize,
+                    size:'small',
+                    onPageClicked: function(a,b,c,p){
+                        page = p;
+                        render();
+                    }
+                })
+            }
         })
     }
     render();
+
+    //添加分类按钮添加点击事件
+    $('.btn-fl').on('click',function(){
+
+        //显示模态框
+        $('#addModal').modal('show');
+
+    });
 
 });
